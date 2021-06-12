ecb996f47e3ac47b0012252e52c1e7a12dab5fe0
peachberry <peachberry0318@gmail.com>
2021-06-12 21:53:04 +0900

    initial commit

--- Main/AdminPage.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("i");
-const sidebar = document.querySelector("#nav");
-
-function sidebarClickEvent(event) {
-   sidebar.classList.toggle('active');
-}
-
-button.addEventListener("click", sidebarClickEvent);
